160664aa7a98f3e2160eee080f6d19ba2fb9bad9
chikamasuda <c.m.062820@gmail.com>
2022-02-16 23:57:44 +0900

    ジョブの実装完了

--- classes/Enemy.php
+++ classes/Enemy.php
@@ -1,24 +1,44 @@
 <?php
 class Enemy
 {
-  const MAX_HITPOINT = 50; //最大HPの定義 定数
-  public $name; //敵の名前
-  public $hitpoint = 50; //現在のHP
-  public $attackPoint = 10; //攻撃力
-  
-  public function doAttack($human)
-  {
-    echo "『" . $this->name . "』の攻撃！\n";
-    echo "『" . $human->name . "』に" . $this->attackPoint . "のダメージ!\n";
-    $human->tookDamage($this->attackPoint);
-  }
+    const MAX_HITPOINT = 50; //最大HPの定義 定数
+    private $name; //敵の名前
+    private $hitPoint = 50; //現在のHP
+    private $attackPoint = 10; //攻撃力
 
-  public function tookDamage($damage)
-  {
-    $this->hitPoint -= $damage;
-    //HPが0未満にならないための処理
-    if ($this->hitPoint < 0) {
-      $this->hitPoint = 0;
+    public function __construct($name)
+    {
+        $this->name = $name;
     }
-  }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getHitPoint()
+    {
+        return $this->hitPoint;
+    }
+
+    public function getAttackPoint()
+    {
+        return $this->attackPoint;
+    }
+
+    public function doAttack($human)
+    {
+        echo "『" . $this->getName() . "』の攻撃！\n";
+        echo "『" . $human->getName() . "』に" . $this->attackPoint . "のダメージ!\n";
+        $human->tookDamage($this->attackPoint);
+    }
+
+    public function tookDamage($damage)
+    {
+        $this->hitPoint -= $damage;
+        //HPが0未満にならないための処理
+        if ($this->hitPoint < 0) {
+            $this->hitPoint = 0;
+        }
+    }
 }
--- main.php
+++ main.php
@@ -1,3 +1,33 @@
 <?php
 
 //echo "処理のはじまりはじまり〜!\n\n";
+//ファイルのロード
+require_once('./classes/Human.php');
+require_once('./classes/Enemy.php');
+require_once('./classes/Brave.php');
+
+//インスタンス化
+$tiida = new Brave("ティーダ");
+$goblin = new Enemy("ゴブリン");
+
+$turn = 1;
+
+while ($tiida->getHitPoint() > 0 && $goblin->getHitPoint() > 0) {
+    echo "*** $turn ターン目 ***\n\n"; //ここを追加
+
+    //現在のHPの表示
+    echo $tiida->getName() . "：" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
+    echo $goblin->getName() . "：" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
+
+    //攻撃
+    $tiida->doAttack($goblin);
+    echo "\n";
+    $goblin->doAttack($tiida);
+    echo "\n";
+
+    $turn++;
+}
+
+echo "★★★ 戦闘終了 ★★★\n\n";
+echo $tiida->getName() . ":" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->getName() . ":" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n\n";
